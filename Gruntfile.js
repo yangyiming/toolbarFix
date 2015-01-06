8f277002da6c295b01ee58255845f6ed47c8dc6e
yangyiming <529710127@qq.com>
2015-01-06 17:41:25 +0800

    bower

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,35 +1,15 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-        options: {
-            banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
-        },
-        dist: {
-            src: 'src/toolbarFix.js',
-            dest: 'dist/toolbarFix.min.js'
-        }
-    },
-      concat: {
-          options: {
-              banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
-          },
-          dist: {
-              src: 'src/toolbarFix.js',
-              dest: 'dist/toolbarFix.js'
-          }
-      },
       'jsmin-sourcemap': {
           all: {
-              src: ['dist/toolbarFix.js'],
-              dest: 'dist/toolbarFix.min.js',
-              destMap: 'dist/toolbarFix.js.map'
+              src: ['toolbarFix.js'],
+              dest: 'toolbarFix.min.js',
+              destMap: 'toolbarFix.js.map'
           }
       }
 
   });
-   grunt.loadNpmTasks('grunt-contrib-uglify');
-   grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-jsmin-sourcemap');
-  grunt.registerTask('default', [ 'uglify:dist','concat:dist','jsmin-sourcemap']);
+  grunt.registerTask('default', ['jsmin-sourcemap']);
 };
